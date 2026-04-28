c941c536009371dc153c879a6917e48408957ef2
Orlando <orlandomeza1821@gmail.com>
2026-04-27 22:52:21 -0500

    feat: implementar microservicio de servicios con JPA, logs y CORS (v85)

---
+++ src/main/java/com/hotelsbook/services/repository/HotelServiceRepository.java
@@ -0,0 +1,19 @@
+package com.hotelsbook.services.repository;
+
+import com.hotelsbook.services.entity.HotelServiceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HotelServiceRepository extends JpaRepository<HotelServiceEntity, Long>{
+	
+	@Query(value = "CALL GetServicesByHotels(:hotelIds)", nativeQuery = true)
+    //@Procedure(procedureName = "GetServicesByHotels")
+    List<Object[]> getServicesByHotels(@Param("hotelIds") String hotelIds);
+
+}
